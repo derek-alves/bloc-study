c9c590372f2c7001dced8e4986d58ee7a6dd7133
Derek Enrique Alves <derek.alves@usemobile.xyz>
2022-01-08 22:09:02 -0300

    first login bloc implementation

---
+++ lib/login/bloc/authentication_bloc.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:bloc_study/login/login.dart';
+import 'package:equatable/equatable.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository _authenticationRepository;
+  final UserRepository _userRepository;
+  late StreamSubscription<AuthenticationStatus> _authenticationStatusSubscription;
+
+  AuthenticationBloc({
+    required AuthenticationRepository authenticationRepository,
+    required UserRepository userRepository,
+  })  : _authenticationRepository = authenticationRepository,
+        _userRepository = userRepository,
+        super(const AuthenticationState.unknown()) {
+    on<AuthenticationStatusChanged>(_onAuthenticationStatusChanged);
+    on<AuthenticationLogoutRequested>(_onAuthenticationLogoutRequested);
+    _authenticationStatusSubscription = _authenticationRepository.status.listen(
+      (status) => add(AuthenticationStatusChanged(status)),
+    );
+  }
+
+  @override
+  Future<void> close() {
+    _authenticationStatusSubscription.cancel();
+    _authenticationRepository.dispose();
+    return super.close();
+  }
+
+  void _onAuthenticationStatusChanged(
+    AuthenticationStatusChanged event,
+    Emitter<AuthenticationState> emit,
+  ) async {
+    switch (event.status) {
+      case AuthenticationStatus.unauthenticated:
+        return emit(const AuthenticationState.unauthenticated());
+      case AuthenticationStatus.authenticated:
+        final user = await _tryGetUser();
+        return emit(
+            user != null ? AuthenticationState.authenticated(user) : const AuthenticationState.unauthenticated());
+      default:
+        return emit(const AuthenticationState.unknown());
+    }
+  }
+
+  void _onAuthenticationLogoutRequested(
+    AuthenticationLogoutRequested event,
+    Emitter<AuthenticationState> emit,
+  ) {
+    _authenticationRepository.logOut();
+  }
+
+  Future<User?> _tryGetUser() async {
+    try {
+      await _userRepository.getUser().then((user) {
+        return user;
+      });
+    } catch (_) {
+      return null;
+    }
+  }
+}
---
+++ lib/login/login.dart
@@ -0,0 +1,2 @@
+export 'packages/authentication_repository/lib/authentication.dart';
+export 'packages/user_repository/lib/user.dart';
